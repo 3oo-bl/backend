6c42b80334a939485b1b1897e6526a44961dd235
Foma Medvedev <tolikslonov5@gmail.com>
2026-05-04 00:03:19 +0500

    polling service HUGE refactoring (partial cache/markets)

--- ProfitableViewApp/DTOS/RequestStartItem.cs
+++ ProfitableViewApp/DTOS/RequestStartItem.cs
@@ -1,8 +1,8 @@
 namespace ProfitableViewApp.DTOS;
 
-public class RequestStartDTO
+public class RequestStart
 {
     public string Item { get; set; }
-    public string[]? Markets { get; set; }
+    public string[] Markets { get; set; }
     public int? Quantity { get; set; }
 }
